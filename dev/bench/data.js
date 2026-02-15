86a8acddb2b57d61fe93166d9728aee116b69baf
github-action-benchmark <github@users.noreply.github.com>
2026-02-15 11:25:18 +0000

    add Benchmark (cargo) benchmark result for b2e05d3db1c6cca99ca209855ba14136bda858d6

---
+++ dev/bench/data.js
@@ -0,0 +1,50 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1771154718405,
+  "repoUrl": "https://github.com/wowemulation-dev/cascette-rs",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "daniel@kogito.network",
+            "name": "Daniel S. Reichenbach",
+            "username": "danielsreichenbach"
+          },
+          "committer": {
+            "email": "daniel@kogito.network",
+            "name": "Daniel S. Reichenbach",
+            "username": "danielsreichenbach"
+          },
+          "distinct": true,
+          "id": "b2e05d3db1c6cca99ca209855ba14136bda858d6",
+          "message": "fix: add write permissions to profiling workflow for gh-pages branch",
+          "timestamp": "2026-02-15T18:21:32+07:00",
+          "tree_id": "65f26ead7e32d97ad6d02dc4eb1f214b9c92d24b",
+          "url": "https://github.com/wowemulation-dev/cascette-rs/commit/b2e05d3db1c6cca99ca209855ba14136bda858d6"
+        },
+        "date": 1771154717786,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "bpsv_generation/versions/wow",
+            "value": 1868,
+            "range": "± 68",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "bpsv_generation/cdns/wow",
+            "value": 1275,
+            "range": "± 20",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "bpsv_generation/summary/all",
+            "value": 204,
+            "range": "± 2",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
